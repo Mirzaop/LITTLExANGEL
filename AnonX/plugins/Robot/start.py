a9d8f71fc87f3a46c1a9f1dec3cabb0a1186ceed
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-08-17 00:42:03 +0530

    Update start.py

--- AnonX/plugins/Robot/start.py
+++ AnonX/plugins/Robot/start.py
@@ -188,7 +188,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAIjTGKPYCq3keRZgNbshxtJ5k7H609OAAIZBgACYAF5VIerYoMcSln8JAQ")
+                await message.reply_sticker("CAACAgUAAx0CaqL1MAADtWL76302lflI5o5XxZFMKz-Yjc0TAAKlBQAChCbRVlk9urYMfCJSHgQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
